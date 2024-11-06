9b96153d37eed1425190d4f749daa6a3477665c7
amielpoletti <162723122+amielpoletti@users.noreply.github.com>
2024-11-05 22:38:28 -0300

    Update set_loops.py

--- set_loops.py
+++ set_loops.py
@@ -1,6 +1,7 @@
-def unique_characters(string):
+def unique_strings(string):
     unique_chars = set()
     for char in string:
         if char not in unique_chars:
             unique_chars.add(char)
     return unique_chars
+
